634e613c6ddf682a307e14b7bcd0b6cf040d8fe6
Vedhachala T S <Vedhachala.TShanmugam@microfocus.com>
2020-06-21 15:21:20 +0530

    Initial Commit

---
+++ server/src/test/java/com/lockdown/clique/CliqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lockdown.clique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
